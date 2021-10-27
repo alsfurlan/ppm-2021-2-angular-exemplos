175ae1fb66aa45b45425ebb5beb830b36107a634
Anderson Furlan <alsfurlan@gmail.com>
2021-10-26 21:27:17 -0300

    Componente de data e hora

---
+++ src/app/data-hora/data-hora.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-hora',
+  templateUrl: './data-hora.component.html',
+  styleUrls: ['./data-hora.component.css']
+})
+export class DataHoraComponent implements OnInit {
+
+  dataHora: Date;
+
+  constructor() {
+    this.dataHora = new Date();
+    setInterval(() => {
+      this.dataHora = new Date();
+    }, 1000);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
